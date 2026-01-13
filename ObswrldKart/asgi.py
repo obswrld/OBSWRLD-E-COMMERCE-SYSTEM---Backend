96edb7534178d6f4a5c94cc0c2156728b59222df
Republic Oba <republicoba1@gmail.com>
2026-01-13 17:19:46 +0100

    corrected works on the setup

--- ObswrldKart/asgi.py
+++ ObswrldKart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ObswrldCart project.
+ASGI config for ObswrldKart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ObswrldCart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ObswrldKart.settings')
 
 application = get_asgi_application()
